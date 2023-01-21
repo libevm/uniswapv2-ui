622dc7d2c241d2b89be6b35c473ab1d05e9ecfac
John <jooohn.eth@gmail.com>
2023-01-20 19:17:40 -0500

    class to className

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -7,15 +7,15 @@ class NavBar extends Component {
 
   render() {
     return (
-      <nav class="bg-primary-black">
-        <div class="mx-auto max-w-7xl p-2 sm:px-6 lg:px-8">
-          <div class="relative flex h-16 items-center justify-between">
-            <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-              <div class="flex items-center text-primary-green font-bold text-2xl">
+      <nav className="bg-primary-black">
+        <div className="mx-auto max-w-7xl p-2 sm:px-6 lg:px-8">
+          <div className="relative flex h-16 items-center justify-between">
+            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+              <div className="flex items-center text-primary-green font-bold text-2xl">
                 Mantle Swap
               </div>
             </div>
-            <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
               {MenuItems.map((item, index) => {
                 return (
                   <li key={index}>
