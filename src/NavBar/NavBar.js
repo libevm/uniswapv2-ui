b358db86bcf25e1ab518bf74dc0d9ec348f6042d
John <jooohn.eth@gmail.com>
2023-01-21 09:07:06 -0500

    Added address display

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,40 +1,53 @@
-import React, { Component } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { MenuItems } from "./MenuItems";
+import { getAccount } from "../ethereumFunctions";
 
-class NavBar extends Component {
-  state = { clicked: false };
+const Navbar = () => {
+  const [address, setAddress] = useState();
 
-  render() {
-    return (
-      <nav className="bg-primary-black">
-        <div className="mx-auto max-w-7xl p-2 sm:px-6 lg:px-8">
-          <div className="relative flex h-16 items-center justify-between">
-            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-              <div className="flex items-center text-primary-green font-bold text-2xl">
-                Mantle Swap
-              </div>
-            </div>
-            <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-              {MenuItems.map((item, index) => {
-                return (
-                  <li key={index}>
-                    <Link
-                      className="text-white underline underline-offset-8 p-3 text-sm font-medium"
-                      aria-current="page"
-                      to={item.url}
-                    >
-                      {item.title}
-                    </Link>
-                  </li>
-                );
-              })}
-            </div>
+  useEffect(() => {
+    const fetchAccount = async () => {
+      await getAccount().then(async (result) => {
+        let first = result.slice(0, 6);
+        let second = result.slice(result.length - 5, result.length - 1);
+
+        setAddress(first.concat("...", second));
+      });
+    };
+
+    fetchAccount();
+  }, [address]);
+
+  return (
+    <nav className="bg-primary-gray">
+      <div className="mx-auto max-w-7xl p-2 sm:px-6 lg:px-8">
+        <div className="relative flex h-16 items-center justify-around">
+          <div className="flex items-center justify-center sm:items-stretch sm:justify-start text-primary-green font-bold text-2xl">
+            Mantle Swap
+          </div>
+          <div className="flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 mx-auto">
+            {MenuItems.map((item, index) => {
+              return (
+                <li className="list-none" key={index}>
+                  <Link
+                    className="no-underline text-primary-green no-underline p-3 text-sm font-bold"
+                    aria-current="page"
+                    to={item.url}
+                  >
+                    {item.title}
+                  </Link>
+                </li>
+              );
+            })}
+          </div>
+          <div className="bg-primary-green border-none outline-none px-4 py-2 font-poppins font-medium text-base text-white rounded-3xl leading-[24px] transition-all">
+            {address}
           </div>
         </div>
-      </nav>
-    );
-  }
-}
+      </div>
+    </nav>
+  );
+};
 
-export default NavBar;
+export default Navbar;
